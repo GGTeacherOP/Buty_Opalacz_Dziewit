b16414177e1d9792cb1587c963909f42597c4625
opalaczoliwia <opalaczoliwiatp@gmail.com>
2025-05-13 19:03:08 +0200

    dodanie stylu + dodanie pytan z odpowiedzia rozwijalna, dodanie skryptu ktory wysyla pytanie uzytkownika do bazy danych

--- kontakt.php
+++ kontakt.php
@@ -88,6 +88,40 @@ form button:hover {
   font-size:1.3rem;
   padding:0.83rem;
 }
+    .zbior {
+      border-bottom: 1px solid #ddd;
+      padding: 10px 0;
+    }
+
+    .pytanie {
+        font-size: larger;
+      font-weight: bold;
+      cursor: pointer;
+      position: relative;
+    }
+
+    .pytanie::after {
+      content: "▼";
+      position: absolute;
+      right: 0;
+      transition: transform 0.3s;
+    }
+
+    .pytanie.active::after {
+      transform: rotate(180deg);
+    }
+
+    .odpowiedz {
+      display: none;
+      padding-top: 10px;
+    }
+
+    .odpowiedz.show {
+      display: block;
+    }
+    p{
+        font-size: large;
+    }
     </style>
 </head>
 <body>
@@ -124,13 +158,69 @@ form button:hover {
                 Prosimy o wskazanie numeru zamówienia oraz tematu sprawy w tytule wiadomości - pomoże nam to szybciej i skuteczniej zająć się Twoim zgłoszeniem.<br><br>
                 Dziękujemy za zrozumienie i prosimy o cierpliwość.</p>
             </div><br><br><br>
+            <div class="zbior">
+    <div class="pytanie">Kiedy dostanę zwrot pieniędzy?</div>
+    <div class="odpowiedz">
+      <p><strong>Zwrot w sklepie internetowym</strong><br>
+      Pieniądze otrzymasz maksymalnie do 14 dni. Zazwyczaj jest to kilka dni roboczych.</p><br>
+      
+      <p><strong>Zwrot produktów opłaconych metodą PayPo</strong><br>
+      Pieniądze otrzymasz przez serwis PayPo. Jeśli zwrot produktów nastąpił w ciągu 30 dni od zakupu, PayPo zaktualizuje Twoje saldo w ciągu 5 dni roboczych.</p><br>
+      
+      <p><strong>Zwrot produktów opłaconych Kartą Podarunkową</strong><br>
+      Pieniądze otrzymasz na nową Kartę Podarunkową, którą wyślemy do Ciebie mailem.</p><br>
+
+      <p><strong>Zwrot w sklepie stacjonarnym</strong><br>
+      Zwrot środków nastąpi w formie doładowania Karty Podarunkowej CCCxHalfPrice według zasad z Regulaminu Kart Podarunkowych. W razie awarii terminy mogą się wydłużyć o kolejne 5 dni roboczych.</p>
+    </div>
+  </div>
+
+  <div class="zbior">
+    <div class="pytanie">Jak mogę złożyć reklamację?</div>
+    <div class="odpowiedz">
+      <p>Aby złożyć reklamację, skontaktuj się z działem obsługi klienta lub odwiedź najbliższy sklep stacjonarny.</p>
+    </div>
+  </div>
+
+  <div class="zbior">
+    <div class="pytanie">Co zrobić jeśli przerwało mi płatność?</div>
+    <div class="odpowiedz">
+      <p>Sprawdź status transakcji w historii płatności. Jeśli płatność nie została pobrana, możesz złożyć zamówienie ponownie.</p>
+    </div>
+  </div>
+
+  <div class="zbior">
+    <div class="pytanie">Jak mogę zwrócić produkt?</div>
+    <div class="odpowiedz">
+      <p>Nasze zwroty są szybkie i proste:<br> 
+        - Jeśli zwracasz do 3 produktów włącznie, skorzystaj z InPost Paczkomat® 24/7,<br>
+        - Jeśli zwracasz 4 lub więcej produktów, zwrócisz je za pośrednictwem Kuriera DPD.<br><br>
+    Każdy produkt możesz też zwrócić w najbliższym sklepie stacjonarnym.<br><br>
+    Chcesz zwrócić produkt przez internet?<br>
+    1. Zaloguj się i znajdź zakładkę zwroty na Twoim koncie. <br> 
+    2. Wybierz, które produkty chcesz zwrócić.  <br>
+    3. Idź do Paczkomatu® albo czekaj na kuriera.  <br><br>
+    Jeśli zwracasz produkty Paczkomatem®, wyślemy do Ciebie SMS z kodem do dowolnego Paczkomatu®. Nie musisz drukować etykiety.<br>
+    Jeśli zwracasz Kurierem DPD podaj dokładny adres, z którego chcesz nadać paczkę. Później zmiana adresu nie będzie możliwa.</p>
+    </div>
+  </div>
+
+  <script>
+    document.querySelectorAll('.pytanie').forEach(item => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('active');
+        const answer = item.nextElementSibling;
+        answer.classList.toggle('show');
+      });
+    });
+  </script><br><br>
             <section class="kontakt">
             <h1>Czy masz inne pytania? Napisz do nas</h1>
             <form action="" method="post">
-            <input type="text" name="name" placeholder="Twoje imię" required>
+            <input type="text" name="imie" placeholder="Twoje imię" required>
             <input type="email" name="email" placeholder="Twój email" required>
-            <textarea name="message" placeholder="Napisz tutaj swoje pytanie" required></textarea>
-            <button type="submit" class="submit">Wyślij</button>
+            <textarea name="wiad" placeholder="Napisz tutaj swoje pytanie" required></textarea>
+            <button type="submit" name="submit" class="submit">Wyślij</button>
             </form>
             </section>
             <div class="info">
@@ -140,6 +230,25 @@ form button:hover {
                 info@buty.pl</p>
             </div>
         </div>
+        <?php
+        $con = mysqli_connect('localhost', 'root', '', 'buty');
+        $sql1="SELECT `imie`,`email`,`pytanie`,`email` FROM wiadomosci;";
+        $que1=mysqli_query($con,$sql1);
+        if(isset($_POST['submit']) && $_POST['imie'] && isset($_POST['email']) && isset($_POST['wiad'])){
+            $imie = $_POST['imie'];
+            $email = $_POST['email'];
+            $wiad = $_POST['wiad'];
+            $sql = "INSERT INTO wiadomosci(`imie`,`email`,`pytanie`) VALUES ('" . $imie . "','" . $email . "','" . $wiad . "');";
+            $que = mysqli_query($con,$sql);
+        }
+        if ($que) {
+        echo "<script>alert('Wiadomość została wysłana do bazy danych!');</script>";
+        } else {
+        echo "<script>alert('Wystąpił błąd podczas zapisu do bazy danych!');</script>";
+        }
+
+        mysqli_close($con);
+        ?>
       </main>
     </div>
     <footer>
