3f0bc120b3c270edc1b26f1f3cf3719f83c8664d
Bdziewit <bartosz7527@wp.pl>
2025-05-18 22:30:34 +0200

    usuwanie pracownika

---
+++ usun_pracownika_admin.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include 'auth_utils.php';
+
+if (!czy_ma_role(['admin', 'szef'])) {
+    header("Location: index.php");
+    exit;
+}
+
+$conn = new mysqli("localhost", "root", "", "buty");
+if ($conn->connect_error) {
+    die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_pracownika'])) {
+    $id_pracownika = $_POST['id_pracownika'];
+
+    $sql = "DELETE FROM pracownicy WHERE id_pracownika = $id_pracownika";
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['komunikat'] = "Pracownik został usunięty.";
+    } else {
+        $_SESSION['error'] = "Błąd usuwania pracownika: " . $conn->error;
+    }
+}
+
+header("Location: pracownicy_admin.php"); // Powrót do listy pracowników
+exit;
+
+$conn->close();
+?>
